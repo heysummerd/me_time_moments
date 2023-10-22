2a70f1ef6ea5e483a2fa9f66a9fa84a8cda0d457
rabujabal <148649201+rabujabal@users.noreply.github.com>
2023-10-22 09:55:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,63 +4,51 @@ import openpyxl
 
 app = Flask(__name__)
 
-# Function to update the Excel database
-def update_excel(user_input):
+# Function to load data from the Excel database
+def load_data():
     excel_file = 'Me Time Moments Activities Database.xlsx'
     workbook = openpyxl.load_workbook(excel_file)
     sheet = workbook.active
-    data = [row[0].value for row in sheet.iter_rows(min_row=2)] 
-
-    new_row = [user_input['activitylevel'], user_input['userInput']]
-    sheet.append(new_row)
-
-    workbook.save(excel_file)
+    data = [row[0].value for row in sheet.iter_rows(min_row=2)]
     workbook.close()
+    return data
 
-# Define a route to display the survey
+# Define a route to display the questionnaire
 @app.route('/')
 def form():
-    return render_template('userInput.html', options=update_excel())
+    return render_template('quiz.html')
 
 # Define a route to handle form submission
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    user_input = {
-        'name': request.form['name'],
-        'userInput': request.form['userInput']
-    }
+    user_selection = request.form['goal']
 
-    update_excel(user_input)
-
-    return redirect(url_for('process_form'))
-
-# Define a route to handle form submission
-@app.route('/process_form', methods=['POST'])
-def process_form():
-    user_selection = request.form['user_selection']
+    data = load_data()
 
     # Filter data based on the user's selection
-    filtered_options = [option for option in update_excel() if user_selection in option]
+    filtered_options = [option for option in data if user_selection in option]
 
     if not filtered_options:
-        random_option = random.choice(update_excel)
+        random_option = random.choice(data)
     else:
         random_option = random.sample(filtered_options, min(len(filtered_options), 3))
 
-    return render_template('userInput.html', options= update_excel, filtered_options=filtered_options, random_option=random_option)
-
-# Define a route to handle random selection
-@app.route('/pick_random', methods=['GET'])
-def pick_random_option():
-    data = update_excel()
-    random_option = random.choice(data)
-    return render_template('userInput.html', random_option=random_option)
+    return render_template('quiz.html', options=data, filtered_options=filtered_options, random_option=random_option)
 
 # Define a route to ask the multiple-choice question
 @app.route('/goal', methods=['GET'])
 def goal():
     return render_template('goal.html')
 
+# Define a route to handle the final submission
+@app.route('/date', methods=['POST'])
+def final_submission():
+    # You can access the user's intention for the date here using request.form['goal']
+    intention = request.form['goal']
+
+    # You can perform any necessary processing or redirection here
+    # For now, let's just display a message with the selected intention
+    return f"Your intention for the date is: {intention}"
 
 # Generate the date
 @app.route('/Your_Date')
